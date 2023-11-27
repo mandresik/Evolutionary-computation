a3bf3b6c9f23cf23c8e064ee23d8ee6d0c21a9b2
mandr <andresikmichal@gmail.com>
2023-11-27 14:49:31 +0100

    bf4

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math as M
 
 
+def norm(x):
+    return M.sqrt(sum(xi**2 for xi in x))
+
+
 def Ackley(x):
     return -20 * M.exp(-0.2 * M.sqrt(sum(xi**2 for xi in x) / len(x))) - M.exp(sum(M.cos(2 * M.pi * xi) for xi in x) / len(x)) + 20 + M.exp(1)
 
@@ -51,4 +55,39 @@ def Mishra11(x):
 
 def Pathological(x): 
     return sum(0.5 + (M.sin(M.sqrt(100 * x[i]**2 + x[i + 1]**2))**2 - 0.5) / (1 + 0.001 * (x[i]**2 - 2 * x[i] * x[i + 1] + x[i + 1]**2)**2) for i in range(len(x) - 1))
+
+def Rastrigin(x): 
+    return 2 * len(x) * sum(xi**2 - 10 * M.cos(2 * M.pi * xi) for xi in x)
+
+def Rosenbrock(x): 
+    return sum(100 * (x[i]**2 - x[i + 1]**2) + (1 - x[i])**2 for i in range(len(x) - 1))
+
+def Salomon(x):
+    norm_x = norm(x)
+    return 1 - M.cos(2 * M.pi * norm_x) + 0.1 * norm_x
+
+def Schwefel22(x):
+    return sum(abs(xi) for xi in x) + M.prod(abs(xi) for xi in x)
+
+def Schwefel26(x):
+    return 418.9829 * len(x) - sum(5 * xi * M.sin(M.sqrt(abs(5 * xi))) for xi in x)
+
+def SineEnvelope(x):
+    w = [x[i]**2 + x[i + 1]**2 for i in range(len(x) - 1)]
+    return -sum(0.5 + (M.sin(wi - 0.5)**2) / (1 + 0.001*(wi))**2 for wi in w)
+
+def StretchedVSine(x):
+    w = [x[i]**2 + x[i + 1]**2 for i in range(len(x) - 1)]
+    return sum(wi**(1/4) * M.sin(50 * wi**(1/10) + 1)**2 for wi in w)
+
+def StyblinskiTang(x):
+    return sum(xi**4 - 16 * xi**2 + 5 * xi for xi in x)
+
+def Trigonometric02(x):
+    w = [(xi - 0.9)**2 for xi in x]
+    return 1 + sum(8 * M.sin(7 * wi)**2 + 6 * M.sin(14 * wi)**2 + wi for wi in w)
+
+def Vincent(x):
+    return -sum(M.sin(10 * M.log10(xi + 101)) for xi in x) / len(x)
+
 
