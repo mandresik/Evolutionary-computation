98f6f607a0cae8646966905dfcaf8f3d992c11b7
mandr <andresikmichal@gmail.com>
2023-11-27 11:17:26 +0100

    bf1_remake

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import math as M
 
 
 def Ackley(x):
-    return -20 * M.exp(-0.2 * M.sqrt(sum(x[i]**2 for i in range(len(x))) / len(x))) - M.exp(sum(M.cos(2 * M.pi * x[i]) for i in range(len(x))) / len(x)) + 20 + M.exp(1)
+    return -20 * M.exp(-0.2 * M.sqrt(sum(xi**2 for xi in x) / len(x))) - M.exp(sum(M.cos(2 * M.pi * xi) for xi in x) / len(x)) + 20 + M.exp(1)
 
-def Alpine01(x):
-    return sum(abs(x[i] * M.sin(x[i]) + 0.1 * x[i]) for i in range(len(x)))
+def Alpine01(x): 
+    return sum(abs(xi * M.sin(xi) + 0.1 * xi) for xi in x)
 
 def Alpine02(x):
-    return M.prod(M.sqrt(x + 100) * M.sin(x + 100))
+    return M.prod(M.sqrt(xi + 100) * M.sin(xi + 100) for xi in x)
 
